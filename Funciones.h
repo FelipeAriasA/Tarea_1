bad034181d6d4e5bbfc7a9a2afa5ea95253b3a0d
FelipeAriasA <felipearias1006@gmail.com>
2021-08-31 02:45:48 -0400

    Archivos base

---
+++ Funciones.h
@@ -0,0 +1,25 @@
+#ifndef Funciones_h
+#define Funciones_h
+#include "List.h"
+
+void importarArchivo(List* listaUsuario, char* archivo);
+
+void exportarArchivo(List listaUsuario, char* archivo);
+
+void agregarCancion(List* listaUsuario, char* nombre, char* artista, char* generos, int ano, char* ListaReproduccion);
+
+void buscarNombre(List* listaUsuario, char* nombre);
+
+void buscarArtista(List* listaUsuario, char* artista);
+
+void buscarGenero(List* listaUsuario, char* genero);
+
+void eliminarCancion(List* listaUsuario, char* nombre);
+
+void mostrarListas(List* listaUsuario);
+
+void cancionesLista(List* listaUsuario, char* listaReproduccion);
+
+void mostrarCanciones(List* lsitaUsuario);
+
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Interfaz.h"
+#include "Funciones.h"
+#include "list.h"
+
+int main(){
+    menu();
+    system("pause");
+    return 0;
+}
